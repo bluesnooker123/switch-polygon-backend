0e8ca4d433ab7493248966e9d168d4226af2a4d4
bluesnooker123 <dev333perfect@gmail.com>
2021-12-04 15:43:44 +0800

    post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Entry Point of the API Server
 
 const express = require('express');
+var cors = require("cors");
 
 /* Creates an Express application.
 The express() function is a top-level
@@ -26,6 +27,7 @@ request stream and exposes it on req.body
 */
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
@@ -44,14 +46,39 @@ pool.connect((err, client, release) => {
 	})
 })
 
-app.get('/testdata', (req, res, next) => {
-	console.log("TEST DATA :");
-	pool.query('Select * from test')
-		.then(testData => {
-			console.log(testData);
-			res.send(testData.rows);
-		})
-})
+// app.get('/confirm', (req, res, next) => {
+// 	console.log("Confirm button pressed!");
+// 	// pool.query('Select * from test')
+// 	// 	.then(testData => {
+// 	// 		console.log(testData);
+// 	// 		res.send(testData.rows);
+// 	// 	})
+// })
+
+app.post('/confirm', function(req, res){
+    console.log(req.body);
+
+    const Web3 = require('web3');
+    const NODE_URL = "wss://speedy-nodes-nyc.moralis.io/4d4d3f866566390af4c30378/polygon/mumbai/ws";    
+    const web3 = new Web3(NODE_URL);
+
+    const CONTRACT_ADDRESS = "0x3702f4c46785bbd947d59a2516ac1ea30f2babf2";
+    const revenueContract = new web3.eth.Contract([{ "inputs": [{ "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "id", "type": "uint256" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }], CONTRACT_ADDRESS);
+    
+    revenueContract.methods.balanceOf("111111", 1)
+    // revenueContract.methods.getRandNum4LootBox().call()
+    // .then(function(){
+    //   //revenueContract.methods.openLootBox()
+    // })
+  
+    // fs.appendFile('public/OrderCheckerLog.txt', req.body.logdata + '\r\n', function (err) {
+    //   if (err) 
+    //     return console.log(err);
+    //   response = { status : "successed" };
+    //   res.end(JSON.stringify(response));
+    // });
+  })
+  
 
 // Require the Routes API
 // Create a Server and run it on the port 3333
